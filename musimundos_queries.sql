0d6d2ecd0833d3279574cfa2006b0ee329ed7143
Juan Jiménez <nefioxdev@gmail.com>
2021-08-01 18:15:55 -0500

    SELECT

--- musimundos_queries.sql
+++ musimundos_queries.sql
@@ -3,3 +3,17 @@ Insertá tu query en mayúsculas. */
 
 INSERT INTO generos(id, nombre)
 VALUES(26, 'Cumbia');
+
+
+/* Viene el manager de Musimundos y, para organizar su inventario de discos, te pide un informe con todos los álbumes que posee la empresa. Realiza una consulta que devuelva todos los álbumes.
+Ingresa de la columna titulo el nombre del álbum que aparece quinto. */
+
+SELECT *
+FROM albumes;
+
+
+/* Para realizar un control de ventas, Musimundos te pide el primer nombre y teléfono de todos sus clientes.
+Insertá el número de teléfono del segundo cliente que obtuviste. */
+
+SELECT primer_nombre, telefono
+FROM clientes;
