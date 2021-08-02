404e91db7359107ba28368517e84c817126e64bb
Juan Jiménez <nefioxdev@gmail.com>
2021-08-01 19:04:01 -0500

    ALIAS

--- musimundos_queries.sql
+++ musimundos_queries.sql
@@ -105,3 +105,8 @@ FROM canciones
 ORDER BY nombre ASC
 LIMIT 5
 OFFSET 5;
+
+/* Decidimos que "nombre" era un poco ambigüo para una columna de tipos de medios. Por eso, Musimundos te pide una lista de los nombres de tipos de medio en una columna que se llame "nombres_de_medios".
+Insertá el primer nombre de medio que obtuviste. */
+SELECT nombre AS nombres_de_medios
+FROM tipos_de_medio;
